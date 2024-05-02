cb95f918a6998abda455be32ba058cd621b8cb2a
Raveen Yasintha <yasintharaveen13@gmail.com>
2024-05-02 11:37:52 +0530

    created customer crud

---
+++ src/main/java/lk/mydentist/api/service/CustomerService.java
@@ -0,0 +1,17 @@
+package lk.mydentist.api.service;
+
+import lk.mydentist.api.dto.CustomerDto;
+
+import java.util.List;
+
+public interface CustomerService {
+    CustomerDto saveCustomer(CustomerDto customerDto);
+
+    List<CustomerDto> getAllCustomer();
+
+    CustomerDto updateCustomer(Long customerId, CustomerDto customerDto);
+
+    CustomerDto deleteCustomer(Long customerId);
+
+    CustomerDto searchCustomer(Long customerId);
+}
---
+++ src/main/java/lk/mydentist/api/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,82 @@
+package lk.mydentist.api.service.impl;
+
+import lk.mydentist.api.dto.CustomerDto;
+import lk.mydentist.api.model.Customer;
+import lk.mydentist.api.repository.CustomerRepo;
+import lk.mydentist.api.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepo customerRepo;
+    private final ModelMapper modelMapper;
+
+    public CustomerServiceImpl(CustomerRepo customerRepo, ModelMapper modelMapper) {
+        this.customerRepo = customerRepo;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public CustomerDto saveCustomer(CustomerDto customerDto) {
+        Customer customer = this.dtoToEntity(customerDto);
+        Customer save = this.customerRepo.save(customer);
+        return entityToDto(save);
+    }
+
+    @Override
+    public List<CustomerDto> getAllCustomer() {
+        List<Customer> allCustomer = this.customerRepo.findAllCustomer();
+        List<CustomerDto> list = new ArrayList<>();
+        for (Customer customer : allCustomer){
+            CustomerDto dto = entityToDto(customer);
+            list.add(dto);
+        }
+        return list;
+    }
+
+    @Override
+    public CustomerDto updateCustomer(Long customerId, CustomerDto customerDto) {
+        Optional<Customer> byId = customerRepo.findById(customerId);
+        if (byId.isPresent()){
+            customerDto.setCustomerId(byId.get().getCustomerId());
+            Customer customer = this.dtoToEntity(customerDto);
+            Customer save = customerRepo.save(customer);
+            return entityToDto(save);
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public CustomerDto deleteCustomer(Long customerId) {
+        Optional<Customer> byId = customerRepo.findById(customerId);
+            if (byId.isPresent()){
+                customerRepo.deleteById(customerId);
+                return entityToDto(byId.get());
+            }else {
+                return null;
+            }
+    }
+
+    @Override
+    public CustomerDto searchCustomer(Long customerId) {
+        Optional<Customer> byId = this.customerRepo.findById(customerId);
+        return byId.map(this::entityToDto).orElse(null);
+    }
+
+    private Customer dtoToEntity(CustomerDto dto){
+        Customer customer = modelMapper.map(dto, Customer.class);
+        customer.setCustomerId(dto.getCustomerId());
+        return customer;
+    }
+
+    private CustomerDto entityToDto(Customer customer){
+        return (customer == null) ? null : modelMapper.map(customer, CustomerDto.class);
+    }
+}
